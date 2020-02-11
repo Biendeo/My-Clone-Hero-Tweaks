c8a8a3a0d8818a7404beb227b0d8c7f593070951
Thomas Moffet <pantherman1996@gmail.com>
2020-02-11 17:16:39 +1100

    Updated Perfect Mode to now have the remaining notes indicator.

--- PerfectMode/PerfectMode.cs
+++ PerfectMode/PerfectMode.cs
@@ -35,6 +35,7 @@ namespace PerfectMode {
 		private GUIStyle settingsBoxStyle;
 		private GUIStyle settingsHorizontalSliderStyle;
 		private GUIStyle settingsHorizontalSliderThumbStyle;
+		private Vector2 settingsScrollPosition;
 
 		private string target;
 		private bool isStillFC;
@@ -42,25 +43,15 @@ namespace PerfectMode {
 		private int totalNoteCount;
 		private int currentNoteIndex;
 
+
 		public PerfectMode() {
 			configWindowEnabled = false;
 			configFilePath = new FileInfo(Path.Combine(Environment.CurrentDirectory, "Tweaks", "PerfectModeConfig.xml"));
+			settingsScrollPosition = new Vector2();
 		}
 
 		private void WriteDefaultConfig() {
-			config = new Config {
-				ConfigX = 200.0f,
-				ConfigY = 200.0f,
-				Enabled = false,
-				FC = true,
-				NotesMissed = 0,
-				FailDelay = 2.0f,
-				DisplayImage = true,
-				DisplayImageX = 30.0f,
-				DisplayImageY = Screen.height - 80.0f,
-				DisplayImageColorARGB = Config.ColorToARGB(Color.white),
-				DisplayImageScale = 50
-			};
+			config = new Config();
 			var serializer = new XmlSerializer(typeof(Config));
 			using (var configOut = configFilePath.OpenWrite()) {
 				serializer.Serialize(configOut, config);
@@ -92,6 +83,7 @@ namespace PerfectMode {
 					gameManager = new GameManagerWrapper(gameManagerObject.GetComponent<GameManager>());
 					notes = gameManager.BasePlayers[0].Notes;
 					totalNoteCount = notes.Count;
+					currentNoteIndex = 0;
 					missedNotes = 0;
 				}
 			}
@@ -105,9 +97,6 @@ namespace PerfectMode {
 						}
 						++currentNoteIndex;
 					}
-					//hitNotes = notes.Count(n => n.WasHit);
-					//missedNotes = notes.Count(n => !n.WasHit && n.WasMissed);
-					//missedNotes = notes.Count(n => n.WasMissed && !n.WasHit);
 					if (config.FC && !isStillFC || config.NotesMissed < missedNotes) {
 						failedObjective = true;
 						remainingTimeBeforeRestart = Math.Min(config.FailDelay, (float)(gameManager.SongLength - gameManager.SongTime));
@@ -144,7 +133,7 @@ namespace PerfectMode {
 			}
 			if (configWindowEnabled) {
 				//TODO: Look into why the GUILayout just panics if this and Extra Song UI are both loaded.
-				var outputRect = GUILayout.Window(5318009, new Rect(config.ConfigX, config.ConfigY, 250.0f, 780.0f), OnWindow, new GUIContent("Perfect Mode Settings"), settingsWindowStyle);
+				var outputRect = GUILayout.Window(5318009, new Rect(config.ConfigX, config.ConfigY, 250.0f, 500.0f), OnWindow, new GUIContent("Perfect Mode Settings"), settingsWindowStyle);
 				config.ConfigX = outputRect.x;
 				config.ConfigY = outputRect.y;
 			}
@@ -155,13 +144,25 @@ namespace PerfectMode {
 						var displayImageStyle = new GUIStyle {
 							font = uiFont,
 							fontSize = config.DisplayImageScale,
-							fontStyle = FontStyle.Bold,
+							fontStyle = (config.DisplayImageBold ? FontStyle.Bold : FontStyle.Normal) | (config.DisplayImageItalic ? FontStyle.Italic : FontStyle.Normal),
 							alignment = TextAnchor.UpperLeft,
 							normal = new GUIStyleState {
 								textColor = Config.ARGBToColor(config.DisplayImageColorARGB)
 							}
 						};
 						GUI.Label(new Rect(config.DisplayImageX, config.DisplayImageY, 0.1f, 0.1f), new GUIContent($"{target} mode active"), displayImageStyle);
+					}
+					if (config.RemainingNotesLeft && !config.FC) {
+						var remainingNotesStyle = new GUIStyle {
+							font = uiFont,
+							fontSize = config.RemainingNotesLeftScale,
+							fontStyle = (config.RemainingNotesLeftBold ? FontStyle.Bold : FontStyle.Normal) | (config.RemainingNotesLeftItalic ? FontStyle.Italic : FontStyle.Normal),
+							alignment = TextAnchor.UpperLeft,
+							normal = new GUIStyleState {
+								textColor = Config.ARGBToColor(config.RemainingNotesLeftColorARGB)
+							}
+						};
+						GUI.Label(new Rect(config.RemainingNotesLeftX, config.RemainingNotesLeftY, 0.1f, 0.1f), new GUIContent(config.NotesMissed - missedNotes >= 0 ? $"{config.NotesMissed - missedNotes} note{(config.NotesMissed - missedNotes == 1 ? string.Empty : "s")} can be missed" : $"Too many notes missed"), remainingNotesStyle);
 					}
 					if (failedObjective) {
 						var failedStyle = new GUIStyle {
@@ -181,48 +182,94 @@ namespace PerfectMode {
 		#endregion
 
 		private void OnWindow(int id) {
-			var style = new GUIStyle {
+			var largeLabelStyle = new GUIStyle {
+				fontSize = 20,
+				alignment = TextAnchor.UpperLeft,
+				fontStyle = FontStyle.Bold,
+				normal = new GUIStyleState {
+					textColor = Color.white,
+				}
+			};
+			var smallLabelStyle = new GUIStyle {
 				fontSize = 14,
 				alignment = TextAnchor.UpperLeft,
 				normal = new GUIStyleState {
 					textColor = Color.white,
 				}
 			};
+			settingsScrollPosition = GUILayout.BeginScrollView(settingsScrollPosition);
+
+			GUILayout.Label("Settings", largeLabelStyle);
 			config.Enabled = GUILayout.Toggle(config.Enabled, "Enabled", settingsToggleStyle);
 			config.FC = GUILayout.Toggle(config.FC, "FC Mode", settingsToggleStyle);
-			GUILayout.Label("Note Miss Limit (inclusive)", style);
+			GUILayout.Label("Note Miss Limit (inclusive)", smallLabelStyle);
 			config.NotesMissed = (int)GUILayout.HorizontalSlider(config.NotesMissed, 0.0f, 100.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
 			if (int.TryParse(GUILayout.TextField(config.NotesMissed.ToString(), settingsTextFieldStyle), out int notesMissed)) config.NotesMissed = notesMissed;
-			GUILayout.Label("Fail Delay Before Restart", style);
+			GUILayout.Label("Fail Delay Before Restart", smallLabelStyle);
 			config.FailDelay = GUILayout.HorizontalSlider(config.FailDelay, 0.0f, 10.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
 			if (float.TryParse(GUILayout.TextField(config.FailDelay.ToString(), settingsTextFieldStyle), out float failDelay)) config.FailDelay = failDelay;
-			config.DisplayImage = GUILayout.Toggle(config.DisplayImage, "On-screen indicator", settingsToggleStyle);
-			GUILayout.Label("Indicator X", style);
+
+			GUILayout.Space(25.0f);
+			GUILayout.Label("Indicator", largeLabelStyle);
+			config.DisplayImage = GUILayout.Toggle(config.DisplayImage, "On-screen indicator enabled", settingsToggleStyle);
+			GUILayout.Label("Indicator X", smallLabelStyle);
 			config.DisplayImageX = GUILayout.HorizontalSlider(config.DisplayImageX, -3840.0f, 3840.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
 			if (float.TryParse(GUILayout.TextField(config.DisplayImageX.ToString(), settingsTextFieldStyle), out float displayImageX)) config.DisplayImageX = displayImageX;
-			GUILayout.Label("Indicator Y", style);
+			GUILayout.Label("Indicator Y", smallLabelStyle);
 			config.DisplayImageY = GUILayout.HorizontalSlider(config.DisplayImageY, -2160.0f, 2160.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
 			if (float.TryParse(GUILayout.TextField(config.DisplayImageY.ToString(), settingsTextFieldStyle), out float displayImageY)) config.DisplayImageY = displayImageY;
-			GUILayout.Label("Indicator Scale", style);
+			GUILayout.Label("Indicator Scale", smallLabelStyle);
 			config.DisplayImageScale = (int)GUILayout.HorizontalSlider(config.DisplayImageScale, 0.0f, 500.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
 			if (int.TryParse(GUILayout.TextField(config.DisplayImageScale.ToString(), settingsTextFieldStyle), out int displayImageScale)) config.DisplayImageScale = displayImageScale;
-			var color = Config.ARGBToColor(config.DisplayImageColorARGB);
-			GUILayout.Label("Red", style);
-			color.r = GUILayout.HorizontalSlider(color.r, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
-			if (float.TryParse(GUILayout.TextField(color.r.ToString(), settingsTextFieldStyle), out float r)) color.r = r;
-			GUILayout.Label("Green", style);
-			color.g = GUILayout.HorizontalSlider(color.g, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
-			if (float.TryParse(GUILayout.TextField(color.g.ToString(), settingsTextFieldStyle), out float g)) color.g = g;
-			GUILayout.Label("Blue", style);
-			color.b = GUILayout.HorizontalSlider(color.b, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
-			if (float.TryParse(GUILayout.TextField(color.b.ToString(), settingsTextFieldStyle), out float b)) color.b = b;
-			GUILayout.Label("Alpha", style);
-			color.a = GUILayout.HorizontalSlider(color.a, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
-			if (float.TryParse(GUILayout.TextField(color.a.ToString(), settingsTextFieldStyle), out float a)) color.a = a;
-			config.DisplayImageColorARGB = Config.ColorToARGB(color);
+			config.DisplayImageBold = GUILayout.Toggle(config.DisplayImageBold, "Bold", settingsToggleStyle);
+			config.DisplayImageItalic = GUILayout.Toggle(config.DisplayImageItalic, "Italic", settingsToggleStyle);
+			var displayImageColor = Config.ARGBToColor(config.DisplayImageColorARGB);
+			GUILayout.Label("Red", smallLabelStyle);
+			displayImageColor.r = GUILayout.HorizontalSlider(displayImageColor.r, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(displayImageColor.r.ToString(), settingsTextFieldStyle), out float displayImageR)) displayImageColor.r = displayImageR;
+			GUILayout.Label("Green", smallLabelStyle);
+			displayImageColor.g = GUILayout.HorizontalSlider(displayImageColor.g, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(displayImageColor.g.ToString(), settingsTextFieldStyle), out float displayImageG)) displayImageColor.g = displayImageG;
+			GUILayout.Label("Blue", smallLabelStyle);
+			displayImageColor.b = GUILayout.HorizontalSlider(displayImageColor.b, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(displayImageColor.b.ToString(), settingsTextFieldStyle), out float displayImageB)) displayImageColor.b = displayImageB;
+			GUILayout.Label("Alpha", smallLabelStyle);
+			displayImageColor.a = GUILayout.HorizontalSlider(displayImageColor.a, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(displayImageColor.a.ToString(), settingsTextFieldStyle), out float displayImageA)) displayImageColor.a = displayImageA;
+			config.DisplayImageColorARGB = Config.ColorToARGB(displayImageColor);
+
+			GUILayout.Space(25.0f);
+			GUILayout.Label("Remaining indicator", largeLabelStyle);
+			config.RemainingNotesLeft = GUILayout.Toggle(config.RemainingNotesLeft, "Remaining indicator enabled", settingsToggleStyle);
+			GUILayout.Label("Indicator X", smallLabelStyle);
+			config.RemainingNotesLeftX = GUILayout.HorizontalSlider(config.RemainingNotesLeftX, -3840.0f, 3840.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(config.RemainingNotesLeftX.ToString(), settingsTextFieldStyle), out float remainingNotesLeftX)) config.RemainingNotesLeftX = remainingNotesLeftX;
+			GUILayout.Label("Indicator Y", smallLabelStyle);
+			config.RemainingNotesLeftY = GUILayout.HorizontalSlider(config.RemainingNotesLeftY, -2160.0f, 2160.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(config.RemainingNotesLeftY.ToString(), settingsTextFieldStyle), out float remainingNotesLeftY)) config.RemainingNotesLeftY = remainingNotesLeftY;
+			GUILayout.Label("Indicator Scale", smallLabelStyle);
+			config.RemainingNotesLeftScale = (int)GUILayout.HorizontalSlider(config.RemainingNotesLeftScale, 0.0f, 500.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (int.TryParse(GUILayout.TextField(config.RemainingNotesLeftScale.ToString(), settingsTextFieldStyle), out int remainingNotesLeftScale)) config.RemainingNotesLeftScale = remainingNotesLeftScale;
+			config.RemainingNotesLeftBold = GUILayout.Toggle(config.RemainingNotesLeftBold, "Bold", settingsToggleStyle);
+			config.RemainingNotesLeftItalic = GUILayout.Toggle(config.RemainingNotesLeftItalic, "Italic", settingsToggleStyle);
+			var remainingNotesLeftColor = Config.ARGBToColor(config.RemainingNotesLeftColorARGB);
+			GUILayout.Label("Red", smallLabelStyle);
+			remainingNotesLeftColor.r = GUILayout.HorizontalSlider(remainingNotesLeftColor.r, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(remainingNotesLeftColor.r.ToString(), settingsTextFieldStyle), out float remainingNotesLeftR)) remainingNotesLeftColor.r = remainingNotesLeftR;
+			GUILayout.Label("Green", smallLabelStyle);
+			remainingNotesLeftColor.g = GUILayout.HorizontalSlider(remainingNotesLeftColor.g, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(remainingNotesLeftColor.g.ToString(), settingsTextFieldStyle), out float remainingNotesLeftG)) remainingNotesLeftColor.g = remainingNotesLeftG;
+			GUILayout.Label("Blue", smallLabelStyle);
+			remainingNotesLeftColor.b = GUILayout.HorizontalSlider(remainingNotesLeftColor.b, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(remainingNotesLeftColor.b.ToString(), settingsTextFieldStyle), out float remainingNotesLeftB)) remainingNotesLeftColor.b = remainingNotesLeftB;
+			GUILayout.Label("Alpha", smallLabelStyle);
+			remainingNotesLeftColor.a = GUILayout.HorizontalSlider(remainingNotesLeftColor.a, 0.0f, 1.0f, settingsHorizontalSliderStyle, settingsHorizontalSliderThumbStyle);
+			if (float.TryParse(GUILayout.TextField(remainingNotesLeftColor.a.ToString(), settingsTextFieldStyle), out float remainingNotesLeftA)) remainingNotesLeftColor.a = remainingNotesLeftA;
+			config.RemainingNotesLeftColorARGB = Config.ColorToARGB(remainingNotesLeftColor);
 
 			GUILayout.Space(25.0f);
 			if (GUILayout.Button("Save Config", settingsButtonStyle)) {
+				if (configFilePath.Exists) configFilePath.Delete();
 				var serializer = new XmlSerializer(typeof(Config));
 				using (var configOut = configFilePath.OpenWrite()) {
 					serializer.Serialize(configOut, config);
@@ -233,6 +280,7 @@ namespace PerfectMode {
 			GUILayout.Label($"Perfect Mode v{Assembly.GetExecutingAssembly().GetName().Version.ToString()}");
 			GUILayout.Label("Tweak by Biendeo");
 			GUILayout.Label("Thankyou for using this!");
+			GUILayout.EndScrollView();
 			GUI.DragWindow();
 		}
 	}
--- PerfectMode/Settings/Config.cs
+++ PerfectMode/Settings/Config.cs
@@ -23,6 +23,41 @@ namespace PerfectMode.Settings {
 		public float DisplayImageY;
 		public int DisplayImageScale;
 		public uint DisplayImageColorARGB;
+		public bool DisplayImageBold;
+		public bool DisplayImageItalic;
+
+		public bool RemainingNotesLeft;
+		public float RemainingNotesLeftX;
+		public float RemainingNotesLeftY;
+		public int RemainingNotesLeftScale;
+		public uint RemainingNotesLeftColorARGB;
+		public bool RemainingNotesLeftBold;
+		public bool RemainingNotesLeftItalic;
+
+		public Config() {
+			ConfigX = 200.0f;
+			ConfigY = 200.0f;
+			Enabled = false;
+			FC = true;
+			NotesMissed = 0;
+			FailDelay = 2.0f;
+
+			DisplayImage = true;
+			DisplayImageX = 30.0f;
+			DisplayImageY = Screen.height - 80.0f;
+			DisplayImageBold = true;
+			DisplayImageItalic = false;
+			DisplayImageColorARGB = Config.ColorToARGB(Color.white);
+			DisplayImageScale = 50;
+
+			RemainingNotesLeft = true;
+			RemainingNotesLeftX = 30.0f;
+			RemainingNotesLeftY = Screen.height - 130.0f;
+			RemainingNotesLeftBold = false;
+			RemainingNotesLeftItalic = true;
+			RemainingNotesLeftColorARGB = Config.ColorToARGB(Color.white);
+			RemainingNotesLeftScale = 40;
+		}
 
 		public static uint ColorToARGB(Color color) => (((uint)(color.a * 255.0f) & 0xFF) << 24) | (((uint)(color.r * 255.0f) & 0xFF) << 16) | (((uint)(color.g * 255.0f) & 0xFF) << 8) | ((uint)(color.b * 255.0f) & 0xFF);
 
